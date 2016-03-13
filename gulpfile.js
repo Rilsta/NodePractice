f84978137ee7a9091888a88fc2ef6d0b425e5f23
rilsta <rstarnes9@gmail.com>
2016-03-13 12:12:17 -0700

    add minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
-var concat = require('gulp-concat');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
 
-gulp.task('concatInterface', function(){
-  return gulp.src(['./js/browser.js', './js/signup-interface.js'])
-  .pipe(concat('allConcat.js'))
-  .pipe(gulp.dest('./tmp'));
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
 });
 
 gulp.task('jsBrowserify', ['concatInterface'] , function() {
@@ -14,4 +15,10 @@ gulp.task('jsBrowserify', ['concatInterface'] , function() {
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
 });
