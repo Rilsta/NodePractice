60ee83a1802307c1d75cfee3e71503e22d8540ed
rilsta <rstarnes9@gmail.com>
2016-03-13 12:03:02 -0700

    add gul[

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var concat = require('gulp-concat');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var gulp = require('gulp');
+
+gulp.task('concatInterface', function(){
+  return gulp.src(['./js/browser.js', './js/signup-interface.js'])
+  .pipe(concat('allConcat.js'))
+  .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify', ['concatInterface'] , function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
